61df624c71a776bbe7ee76fc06f54d9ced8f2da5
Mohammed Eladawi <mohammedeladwe3@gmail.com>
2026-03-07 16:50:18 +0400

    added response and response handler for consistency

--- SchoolProject.Core/Features/Students/Queries/Handlers/StudentHandler.cs
+++ SchoolProject.Core/Features/Students/Queries/Handlers/StudentHandler.cs
@@ -1,12 +1,15 @@
 using AutoMapper;
 using MediatR;
+using SchoolProject.Core.Bases;
 using SchoolProject.Core.Features.Students.Queries.Models;
 using SchoolProject.Core.Features.Students.Queries.Responses;
 using SchoolProject.Service.Abstracts;
 
 namespace SchoolProject.Core.Features.Students.Queries.Handlers;
 
-public class StudentHandler : IRequestHandler<GetAllStudentsQuery, List<StudentDtoForList>>
+public class StudentHandler :
+    ResponseHandler,
+    IRequestHandler<GetAllStudentsQuery, Response<List<StudentDtoForList>>>
 {
     private readonly IStudentService _studentService;
     private readonly IMapper _mapper;
@@ -17,10 +20,12 @@ public class StudentHandler : IRequestHandler<GetAllStudentsQuery, List<StudentD
         this._mapper = mapper;
     }
 
-    public async Task<List<StudentDtoForList>> Handle(GetAllStudentsQuery request, CancellationToken cancellationToken)
+    public async Task<Response<List<StudentDtoForList>>> Handle(GetAllStudentsQuery request, CancellationToken cancellationToken)
     {
         var studentsList = await _studentService.GetAllStudentsAsync();
-        return _mapper.Map<List<StudentDtoForList>>(studentsList);
+        var studentsDtoList = _mapper.Map<List<StudentDtoForList>>(studentsList);
+
+        return Created(studentsDtoList);
 
     }
 }
--- SchoolProject.Core/Features/Students/Queries/Models/GetAllStudentsQuery.cs
+++ SchoolProject.Core/Features/Students/Queries/Models/GetAllStudentsQuery.cs
@@ -1,10 +1,10 @@
 using MediatR;
+using SchoolProject.Core.Bases;
 using SchoolProject.Core.Features.Students.Queries.Responses;
-using SchoolProject.Data.Entities;
 
 namespace SchoolProject.Core.Features.Students.Queries.Models;
 
-public class GetAllStudentsQuery : IRequest<List<StudentDtoForList>>
+public class GetAllStudentsQuery : IRequest<Response<List<StudentDtoForList>>>
 {
 
 }
