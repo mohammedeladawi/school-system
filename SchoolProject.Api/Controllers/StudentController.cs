64f16d28216fc206ff11e671ae6f234892a66057
Mohammed Eladawi <mohammedeladwe3@gmail.com>
2026-03-10 19:47:40 +0400

    added AppControllerBase for related code "DRY"

--- SchoolProject.Api/Controllers/StudentController.cs
+++ SchoolProject.Api/Controllers/StudentController.cs
@@ -1,38 +1,32 @@
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using SchoolProject.Api.Base;
 using SchoolProject.Core.Features.Students.Commands.Models;
 using SchoolProject.Core.Features.Students.Queries.Models;
 using SchoolProject.Data.AppMetaData;
 
 namespace SchoolProject.Api.Controllers;
 
-public class StudentController : ControllerBase
+public class StudentController : AppControllerBase
 {
-    private readonly IMediator _mediator;
-
-    public StudentController(IMediator mediator)
-    {
-        this._mediator = mediator;
-    }
-
     [HttpGet(Router.Student.List)]
     public async Task<IActionResult> GetAllStudents()
     {
-        var students = await _mediator.Send(new GetAllStudentsQuery());
-        return Ok(students);
+        var students = await Mediator.Send(new GetAllStudentsQuery());
+        return NewResult(students);
     }
 
     [HttpGet(Router.Student.GetById)]
     public async Task<IActionResult> GetStudentById(int id)
     {
-        var students = await _mediator.Send(new GetSingleStudentQuery() { Id = id });
-        return Ok(students);
+        var students = await Mediator.Send(new GetSingleStudentQuery() { Id = id });
+        return NewResult(students);
     }
 
     [HttpPost(Router.Student.Add)]
     public async Task<IActionResult> AddStudent([FromBody] AddStudentCommand command)
     {
-        var result = await _mediator.Send(command);
-        return Ok(result);
+        var result = await Mediator.Send(command);
+        return NewResult(result);
     }
 }
